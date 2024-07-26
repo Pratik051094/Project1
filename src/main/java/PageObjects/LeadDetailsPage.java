b7d959df854453815026d441b84b7355c15701ed
ankushsingh-planit <ankushkumar.singh@planittesting.com>
2024-07-26 13:36:02 +0000

    new changes

--- src/main/java/PageObjects/LeadDetailsPage.java
+++ src/main/java/PageObjects/LeadDetailsPage.java
@@ -3,20 +3,8 @@ package PageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-<<<<<<< HEAD
-public class LeadDetailsPage {
-	WebDriver driver;
-
-	LeadDetailsPage(WebDriver driver) {
-
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-	}
-=======
 public class LeadDetailsPage extends DropDown {
->>>>>>> cb65e31710f33c0dab40787f2205c539710d34a3
 
 	@FindBy(xpath = "(//input[@class='slds-input'])[2]")
 	private WebElement fstName;
@@ -36,4 +24,5 @@ public class LeadDetailsPage extends DropDown {
 	@FindBy(xpath = "//button[@class='slds-button slds-button_brand']")
 	private WebDriver snp;
 
+	
 }
