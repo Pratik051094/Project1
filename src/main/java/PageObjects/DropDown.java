fbef2692f170bed824a54f60e6921522b38ca051
ankushsingh-planit <ankushkumar.singh@planittesting.com>
2024-07-26 12:28:31 +0530

    Update DropDown.java

--- src/main/java/PageObjects/DropDown.java
+++ src/main/java/PageObjects/DropDown.java
@@ -1,1 +1,16 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class DropDown {
+
+	WebDriver driver;
+
+	public void DrpDnAccess(String arialabel, String dataVal) {
+		driver.findElement(By.xpath("//button[@aria-label='"+arialabel+"']")).click();
+		WebElement we = driver.findElement(By.xpath("//div[@aria-label='"+arialabel+"']"));
+		we.findElement(By.xpath("//*[@data-value='"+dataVal+"']")).click();
+	}
+}
+
 
